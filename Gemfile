e0a9a23d04e8e5a5ab155a8ed8ce96c5edf9c90f
Islam Shaaban <islamshaaban10@gmail.com>
2021-05-11 00:26:24 +0200

    Lab4

--- Gemfile
+++ Gemfile
@@ -4,7 +4,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '3.0.0'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.2.6'
+gem 'rails', '~> 6.1.3', '>= 6.1.3.2'
+gem 'sqlite3', '~> 1.4'
 # Use postgres as the database for Active Record
 # gem 'pg', '~> 0.18.4'
 # Use Puma as the app server
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,30 @@
 Rails.application.routes.draw do
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'welcome/index'
+
+
+
+  namespace :api do
+    namespace :v1 do
+      resource :users, only: [:create]
+      post "/login", to: "users#login"
+      get "/auto_login", to: "users#auto_login"
+
+      resources :articles do
+        resources :comments
+      end
+    end
+  end
+
+  resources :articles do
+    resources :comments
+  end
+
+  # resources :articles do
+  #   resources :comments
+  # end
+
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
